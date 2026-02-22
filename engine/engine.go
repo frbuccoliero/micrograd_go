50f0e4f5ce2f8a421ac1e5b5a64fd36be19e3789
Francesco Buccoliero <fr.buccoliero@gmail.com>
2026-02-22 19:20:41 +0100

    [+] Working engine

--- engine/engine.go
+++ engine/engine.go
@@ -1,36 +1,110 @@
 package engine
 
 import (
-	"errors"
+	"fmt"
+	"math"
 )
 
 type Value struct{
-	Data float32
-	Grad float32	
+	data float64
+	grad float64
+	backward func()	
+	prev []*Value
+	Name string // making public so i can change it from outside if needed
 }
 
-func NewValue(data float32) Value {
-	return Value{Data: data, Grad: 0.0}
+func NewValue(data float64, prev []*Value, name string) *Value {
+	return &Value{data: data, grad: 0.0, backward: func(){}, prev: prev, Name: name}
 }
 
-func ValidateOperand(op interface{}) (Value, error){
-	_op, ok_op := op.(Value)
-	if !ok_op {
-		_op_val, ok_op_val := op.(float32)
-		if !ok_op_val {
-			return NewValue(0), errors.New("Can't assert type of operand. Enter a Value or float32")
-		} else{
-			return NewValue(_op_val), nil
+func (a *Value) Add(b *Value) *Value {
+	out := NewValue(a.data + b.data, []*Value{a,b}, "+")
+	out.backward = func () {
+		a.grad += out.grad
+		b.grad += out.grad
+	}
+	return out
+}
+
+func (a *Value) Mul(b *Value) *Value {
+	out := NewValue(a.data * b.data, []*Value{a,b}, "*")
+	out.backward = func () {
+		a.grad += b.data * out.grad
+		b.grad += a.data * out.grad
+	}
+	return out
+}
+
+func (a *Value) ReLu() *Value {
+	var res float64
+	if a.data > 0 {
+		res = a.data
+	} else {
+		res = 0.0
+	}
+
+	out := NewValue(res, []*Value{a}, "relu")
+	out.backward = func () {
+		if out.data > 0 {
+			a.grad += out.grad
 		}
 	}
-	return _op, nil
+
+	return out
 }
 
-func Add(a interface{}, b interface{}) Value {
-
-	_a, _ := ValidateOperand(a)
-	_b, _ := ValidateOperand(b)
-
-	out := NewValue(_a.Data + _b.Data)
+func (a *Value) Tanh() *Value {
+	out := NewValue(math.Tanh(a.data), []*Value{a}, "tanh")
+	out.backward = func () {
+		a.grad += (1 - math.Pow(out.data,2)) * out.grad
+	}
 	return out
+}
+
+// sub and div as "negative" operations of add and mul
+
+func (a *Value) Negate() *Value {
+	return a.Mul(NewValue(-1.0, nil, "-1"))
+}
+
+func (a *Value) Invert() *Value {
+	return NewValue(1/a.data, *Value[]{a}, "^-1") // assuming no zero division
+}
+
+func (a *Value) Sub(b *Value) *Value{
+	return a.Add(b.Negate())
+}
+
+func (a *Value) Div(b *Value) *Value {
+	return a.Mul(b.Invert())
+}
+
+func (a *Value) Backward() {
+	// Build topo sort
+	topo := []*Value{}
+	visited := make(map[*Value]bool) // looks like go has no hashset, we use a map
+
+	var helper func((*Value)) 
+	helper = func (a *Value) {
+		if !visited[a] {
+			visited[a] = true
+			for _,p := range a.prev {
+				helper(p)
+			}
+			topo = append(topo, a)
+		}
+	}
+	
+	helper(a)
+	a.grad = 1.0
+
+	// Work in reverse order
+	for idx := len(topo) -1; idx >= 0; idx-- {
+		topo[idx].backward()
+	}
+}
+
+
+func (a *Value) Print() {
+	fmt.Printf("%s [%p] Value(data=%v, grad=%v)\n", a.Name,a, a.data, a.grad)
 }
--- main.go
+++ main.go
@@ -1,14 +1,52 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
 	"micrograd_go/engine"
 )
 
 func main() {
-	a := engine.NewValue(1.0)
-	b := engine.NewValue(2.0)
-    c := engine.Add(a,b)
+	a := engine.NewValue(1.0, nil, "a")
+	b := engine.NewValue(2.0, nil, "b")
+    
+    c := a.Add(b)
+    c.Name = "c"
 
-	fmt.Println(a, b, c)
+    d := engine.NewValue(-2.0, nil, "d")
+
+    e := c.Mul(d)
+    e.Name = "e"
+
+    f := engine.NewValue(3.0, nil, "f")
+
+    g := e.Add(f)
+    g.Name = "g"
+    // g = (a+b)*d + f
+
+    // h := g.ReLu()
+    h := g.Tanh()
+
+	all_vars := []*engine.Value{a,b,c,d,e,f,g,h}
+
+    for _,v := range all_vars {
+        v.Print()
+    }
+
+    h.Backward()
+
+    fmt.Println("---Back pass---")
+
+    // expected on g.Backward()
+    // dg/dg = 1
+    // df/dg = 1
+    // de/dg = 1
+    // dd/dg = dd/de * de/dg = c * 1 = a+b = 3
+    // dc/dg = dc/de * de/dg = d * 1 = -2
+    // da/dg = da/dc * dc/dg = 1 * -2 = -2
+    // db/dg = db/dc * dc/dg = 1 * -2 = -2
+
+    for _,v := range all_vars {
+        v.Print()
+    }
+
 }
